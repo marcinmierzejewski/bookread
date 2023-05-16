95c8996e137059d69f73a1716a5ed09e54a69306
Marcin Mierzejewski <mierzejewski2you@gmail.com>
2023-05-16 23:18:30 +0200

    add select component

--- src/components/Training/MyTraining/MyTraining.jsx
+++ src/components/Training/MyTraining/MyTraining.jsx
@@ -1,9 +1,29 @@
 import { useState } from "react";
+import { useGetAllUserBooksQuery } from "../../../redux/slices/bookApi";
+import Select from "react-select";
 
 export const MyTraining = () => {
+  const {
+    data: { goingToRead } = [],
+    // isLoading,
+    // isSuccess,
+    // isError,
+    // error,
+  } = useGetAllUserBooksQuery();
+
+  const renderLabel = () => {
+    const rendering = [];
+    goingToRead?.map(({ _id, title, author }) => {
+      let label = { value: `${_id}`, label: `${title}: ${author}` };
+      rendering.push(label);
+    });
+    return rendering;
+  };
+
   const [startDate, setStartDate] = useState("");
   const [stopDate, setStopDate] = useState("");
   const [trainingBooks, setTrainingBooks] = useState([]);
+  const [selectBook, setSelectBook] = useState("");
 
   const inputChange = (e) => {
     const { name, value } = e.target;
@@ -16,12 +36,19 @@ export const MyTraining = () => {
   };
 
   const resetForm = () => {
-    startDate("");
-    stopDate("");
+    setStartDate("");
+    setStopDate("");
+  };
+
+  const handleSelectedBook = (selectedBook) => {
+    setSelectBook(selectedBook);
+    console.log(`Selected: ${selectBook.value}`)
   };
 
   const handleAddBooksToTraining = async (e) => {
     e.preventDefault();
+    console.log(startDate, stopDate, trainingBooks);
+    resetForm();
   };
 
   return (
@@ -48,6 +75,18 @@ export const MyTraining = () => {
           placeholder="Finish"
           onChange={inputChange}
         />
+        <Select options={renderLabel()} onChange={handleSelectedBook}/>
+        <div>
+          {goingToRead?.map(({ _id, title, author }) => (
+            <li key={_id}>
+              {title} {author}
+            </li>
+          ))}
+        </div>
+        <button type="submit" onClick={handleAddBooksToTraining}>Add book</button>
+        <div>
+          {selectBook && <>You've selected {selectBook.value}</>}
+        </div>
       </form>
     </>
   );
